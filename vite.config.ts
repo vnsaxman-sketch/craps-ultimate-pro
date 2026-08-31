7b241778bfbe440ee6a44d202586ef82a15fc90e
Long Nguyen <vnsaxman@gmail.com>
2026-08-30 17:39:50 -0700

    Initial React Vite application

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/craps-ultimate-pro/",
+  base: '/craps-ultimate-pro/',
 
   build: {
     outDir: "dist",
